3ba2a3b3199ecf83567d7978d0795c4d2131286b
halt <ha1t@users.noreply.github.com>
2025-03-24 17:25:38 +0900

    fix typo

--- jpg2webp/jpg2webp.go
+++ jpg2webp/jpg2webp.go
@@ -79,7 +79,7 @@ func convertWebP(dir string) {
 		generateFileName := file[0:len(file)-len(ext)] + ".webp"
 		fmt.Println("<Source File Name:", file, ">")
 
-		// cweb
+		// cwebp
 		var cwebpCmd *exec.Cmd
 		if strings.ToLower(ext) == ".jpg" {
 			cwebpCmd = exec.Command("cwebp", "-psnr", "42", "-qrange", "40", "95", "-metadata", "all", "-sharp_yuv", "-o", generateFileName, "-progress", "-short", file)
